58d59a80a3fc8ada94fc1fe97d02eced47c4029b
patrick-n4 <patrickndayambaje4@gmail.com>
2022-06-20 20:21:24 +0200

    I have integrated registers

--- Controllers/handleRegisterUser/handleRegisterUser.php
+++ Controllers/handleRegisterUser/handleRegisterUser.php
@@ -1,8 +1,49 @@
 <?php
-include "../../configs/db.php";
-include_once "../../Middlewares/Register/registerUser.php";
+class User{
+    public $fname;
+    public $lname;
+    public $telephone;
+    public $gender;
+    public $nationality;
+    public $username;
+    public $email;
+    public $password;
+    public $cpassword;
 
+    public function __construct($fname,$lname,$telephone,$gender,$nationality,$username,$email,$password,$cpassword){
+    $this->fname=$fname;
+    $this->lname=$lname;
+    $this->telephone=$telephone;
+    $this->gender=$gender;
+    $this->nationality=$nationality;
+    $this->username=$username;
+    $this->email=$email;
+    $this->password=$password;
+    $this->cpassword=$cpassword;
+    }
+    public function registerUser($connection){
+        if($this->password != $this->cpassword)
+        {
+            echo "Password don't match";
+            return false;
+        }
+        $sql = "INSERT INTO users(firstName, lastName,telephone, gender, nationality,username,email,password) VALUES(:fname,:lname,:telephone,:gender,:nationality,:username,:email ,:password)";
+        $statement = $connection->prepare($sql);
+        $statement->bindParam(':fname', $this->fname);
+        $statement->bindParam(':lname', $this->lname);
+        $statement->bindParam(':telephone', $this->telephone);
+        $statement->bindParam(':gender', $this->gender);
+        $statement->bindParam(':nationality', $this->nationality);
+        $statement->bindParam(':username', $this->username);
+        $statement->bindParam(':email', $this->email);
+        $statement->bindParam(':password', $this->password);
 
-$newUser = new User($_POST['fname'], $_POST['lname'], $_POST['telephone'],$_POST['gender'],$_POST['nationality'],$_POST['username'],$_POST['email'],$_POST['password'],$_POST['cpassword']);
-$newUser->registerUser($connection);
+        try {
+            $statement->execute();
+            echo "User registered successfully";
+        } catch (PDOException $err) {
+            echo "Error ". $err->getMessage();
+        }
+    }
+}
 ?>
--- Middlewares/Register/registerUser.php
+++ Middlewares/Register/registerUser.php
@@ -1,49 +1,8 @@
 <?php
-class User{
-    public $fname;
-    public $lname;
-    public $telephone;
-    public $gender;
-    public $nationality;
-    public $username;
-    public $email;
-    public $password;
-    public $cpassword;
+include "../../configs/db.php";
+include_once "../../Controllers/handleRegisterUser/handleRegisterUser.php";
 
-    public function __construct($fname,$lname,$telephone,$gender,$nationality,$username,$email,$password,$cpassword){
-    $this->fname=$fname;
-    $this->lname=$lname;
-    $this->telephone=$telephone;
-    $this->gender=$gender;
-    $this->nationality=$nationality;
-    $this->username=$username;
-    $this->email=$email;
-    $this->password=$password;
-    $this->cpassword=$cpassword;
-    }
-    public function registerUser($connection){
-        if($this->password != $this->cpassword)
-        {
-            echo "Password don't match";
-            return false;
-        }
-        $sql = "INSERT INTO users(firstName, lastName,telephone, gender, nationality,username,email,password) VALUES(:fname,:lname,:telephone,:gender,:nationality,:username,:email ,:password)";
-        $statement = $connection->prepare($sql);
-        $statement->bindParam(':fname', $this->fname);
-        $statement->bindParam(':lname', $this->lname);
-        $statement->bindParam(':telephone', $this->telephone);
-        $statement->bindParam(':gender', $this->gender);
-        $statement->bindParam(':nationality', $this->nationality);
-        $statement->bindParam(':username', $this->username);
-        $statement->bindParam(':email', $this->email);
-        $statement->bindParam(':password', $this->password);
 
-        try {
-            $statement->execute();
-            echo "User registered successfully";
-        } catch (PDOException $err) {
-            echo "Error ". $err->getMessage();
-        }
-    }
-}
+$newUser = new User($_POST['fname'], $_POST['lname'], $_POST['telephone'],$_POST['gender'],$_POST['nationality'],$_POST['username'],$_POST['email'],$_POST['password'],$_POST['cpassword']);
+$newUser->registerUser($connection);
 ?>
