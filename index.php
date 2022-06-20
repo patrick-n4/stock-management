a0fff2599cc5869b54a8d28683c257fd8aee50d1
patrick-n4 <patrickndayambaje4@gmail.com>
2022-06-20 21:58:36 +0200

    This is the progress

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
 echo "Redirecting..."."<br>";
 echo "<a href='./View/RegisterUser/formUser.php'>Register User</a>";
+echo "<a href='./View/RegisterProduct/formProduct.php' >Register Product</a>";
+
 ?>
